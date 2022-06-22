b34d486b424b4456a66d745cd0c04f8812457375
Nate Herbert <nateherbert@gmail.com>
2022-06-22 14:37:53 -0500

    changed game dict variable name for clarity

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ import math_functions as mf
 number_of_problems = 10
 
 
-def intro_screen():
+def intro_screen() -> int:
+    """provides introduction text to user and returns the game the user would like to play"""
     print(
         f"Welcome to Mini Math! Please select from the options below. You will be given {number_of_problems} math problems and a score at the end.")
 
     game_selected = None
 
-    while game_selected not in problem_list.keys():
+    while game_selected not in game_dict.keys():
         print("Please select from the following options:")
 
-        for k, v in problem_list.items():
+        for k, v in game_dict.items():
             print(k, v[1])
 
         game_selected = user_input()
 
-        while game_selected > len(problem_list.keys()):
+        while game_selected > len(game_dict.keys()):
 
             print("Please select a valid option")
             game_selected = user_input()
@@ -29,7 +30,7 @@ def intro_screen():
 
 def game_loop(game_selected, correct_answers):
 
-    answer = problem_list[game_selected][0]()
+    answer = game_dict[game_selected][0]()
     user_answer = user_input()
 
     if user_answer == answer:
@@ -65,10 +66,10 @@ def results(number_of_problems, correct_answers):
     return input()
 
 
-problem_list = {1: [mf.addition_problem, 'addition'],
-                2: [mf.subtraction_problem, 'subtraction'],
-                3: [mf.multiplication_problem, 'multiplication'],
-                4: [mf.division_problem, 'division']}
+game_dict = {1: [mf.addition_problem, 'addition'],
+             2: [mf.subtraction_problem, 'subtraction'],
+             3: [mf.multiplication_problem, 'multiplication'],
+             4: [mf.division_problem, 'division']}
 
 
 def main():
